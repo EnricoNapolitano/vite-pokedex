8f706d3ae8000e158a12dc26b26e4ea852a03b8d
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-24 16:45:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import "@fontsource/montserrat" //imported font montserrat
+import App from './App.vue'
+
+createApp(App).mount('#app')
