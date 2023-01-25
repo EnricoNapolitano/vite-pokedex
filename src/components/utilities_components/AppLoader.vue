6225d22b1e893b2b900b1357bfa984cfe3efab65
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-25 01:22:23 +0100

    component 'loader'

---
+++ src/components/utilities_components/AppLoader.vue
@@ -0,0 +1,21 @@
+<script></script>
+<template>
+    <!-- loader component -->
+    <div class="loader">
+        <h2><i class="fa-solid fa-spinner fa-spin"></i> LOADING...</h2>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+
+.loader {
+    color: $light-grey;
+    text-align: center;
+
+    h2 {
+        margin-top: 15rem;
+        font-weight: 900;
+        font-size: 10rem;
+    }
+}
+</style>
