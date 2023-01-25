ab9eb386d08b7fad4a36d639567180a1611222c6
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-25 19:05:18 +0100

    created 'utility' component SelectTag

---
+++ src/components/utilities_components/SelectTag.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'SelectTag',
+    data() {
+        return {
+            option: '',
+        }
+    },
+    props: { type: Array },
+    emits: ['change-option']
+}
+</script>
+<template>
+    <select v-model="option" @change="$emit('change-option', option)" name="type" id="type">
+        <option v-for="item in type" :value="item">{{ item }}</option>
+    </select>
+</template>
+<style>
+
+</style>
