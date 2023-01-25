f5d4cf58d239cc5d71d8783490346fb52e879d46
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-25 01:23:17 +0100

    buttons page (just graphic)

---
+++ src/components/utilities_components/ButtonsPage.vue
@@ -0,0 +1,28 @@
+<script></script>
+<template>
+    <div class="cs-btn next">
+        <img src="../../../public/img/next.png" alt="button next">
+    </div>
+    <div class="cs-btn prev">
+        <img src="../../../public/img/prev.png" alt="button previous">
+    </div>
+</template>
+<style scoped>
+.cs-btn {
+    text-align: center;
+    position: absolute;
+    cursor: pointer;
+}
+
+.prev {
+    top: 475px;
+    left: 0;
+    transform: translateX(-90%);
+}
+
+.next {
+    top: 475px;
+    right: 0;
+    transform: translateX(90%);
+}
+</style>
