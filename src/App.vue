29387661a7db9923c1ff59956bf7dae71531ab24
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-25 19:04:14 +0100

    riorganized App: datas, methods

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import axios from 'axios'
 import AppMain from './components/AppMain.vue'
 import { store } from './data/store.js' //imported store object
 export default {
+  data() {
+    return {
+      pokedexUri: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons',
+      pokemonsType: [],
+      SelectedType: '',
+    }
+  },
   components: { AppMain },
   methods: {
     fetchPokemons(uri) {
@@ -13,16 +20,34 @@ export default {
         }).catch().then(() => {
           store.isLoading = false;
         })
+    },
+    fetchPokemonsType(endpoint) {
+      axios.get(endpoint)
+        .then((res) => {
+          this.pokemonsType = res.data;
+          this.pokemonsType.unshift('-- --');
+        })
+    },
+    fetchSameType(word) {
+      this.SelectedType = word;
+      if (word === '-- --') this.fetchPokemons(this.pokedexUri);
+      else {
+        axios.get(`${this.pokedexUri}?eq[Type1]=${word}`)
+          .then((res) => {
+            store.pokemons = res.data.docs;
+          })
+      }
     }
   },
   mounted() {
-    this.fetchPokemons(store.pokedexUri)
+    this.fetchPokemons(this.pokedexUri)
+    this.fetchPokemonsType(`${this.pokedexUri}/types1`)
   }
 }
 </script>
 
 <template>
-  <app-main></app-main>
+  <app-main :type="pokemonsType" @change-option="fetchSameType"></app-main>
 </template>
 
 <style lang="scss">
